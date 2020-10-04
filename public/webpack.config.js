e990f55b95033753ea72eb1830997464aef7fd7f
mc4506 <mike4506@gmail.com>
2020-10-03 23:38:39 -0400

    setup webpack config and webpack-pwa-manifest module

---
+++ public/webpack.config.js
@@ -0,0 +1,31 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+    mode: "development",
+    entry: "./index.js",
+    output: {
+        path: __dirname + "/dist",
+        filename:"bundle.js"
+    },
+    plugins: [
+        new WebpackPwaManifest({
+            name: "Budget Tracker",
+            short_name: "Budget Tracker",
+            description: "An application that tracks your budgets online and offline",
+            background_color: "#ffffff",
+            theme_color: "#ffffff",
+            start_url: "/",
+            display: "standalone",
+            icons: [ {
+                src: path.resolve('icons/icon-192x192.png'),
+                sizes: [72, 192, 512],
+                destination: path.join('assets', 'icons')
+            }]
+
+        })
+    ]
+
+}
+
+module.exports = config;
