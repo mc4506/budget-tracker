a43e5a6740fdfe7b65bc71047f1b66e7bdff4f03
mc4506 <mike4506@gmail.com>
2020-10-04 14:28:28 -0400

    setup indexedDB db.js

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+import './styles.css';
+
 let transactions = [];
 let myChart;
 
--- public/webpack.config.js
+++ public/webpack.config.js
@@ -8,6 +8,14 @@ const config = {
         path: __dirname + "/dist",
         filename:"bundle.js"
     },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+    },
     plugins: [
         new WebpackPwaManifest({
             name: "Budget Tracker",
